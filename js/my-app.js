4a3add5122e57d769681157a5cbaa4efb6040e15
陈航 <512537144@qq.com>
2017-11-14 18:23:57 +0800

    11.14

--- js/my-app.js
+++ js/my-app.js
@@ -123,3 +123,14 @@
   var mySwiper = myApp.swiper('.swiper-container', {
     pagination:'.swiper-pagination'
   });
+
+//标签页切换JS
+  $(".toolbar .tab-link").on('touchstart', function () {
+    $(this).addClass('active').siblings().removeClass('active');
+    var a = $(this).attr('class').replace(/[^0-9]/ig, "");//取得下标数字
+
+    var b = ".tab-" + a;//下标与.tab-组成字符串 选择相应元素
+
+    $(b).addClass('show').siblings().removeClass('show');
+    
+})
